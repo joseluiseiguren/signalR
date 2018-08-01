4e38e8980afd57b1fdf98f29886386c57f0186fb
Jose Luis Eiguren <joseluiseiguren@gmail.com>
2018-08-01 17:34:13 +0200

    initial commit

---
+++ signalR/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using signalR;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Program.Startup))]
+namespace signalR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://+:43666";
+
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                string texto;
+                while ((texto = Console.ReadLine()) != "exit")
+                {
+                    var hub = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+                    hub.Clients.All.newMessage(texto);
+                }
+            }
+        }
+
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.Map("/signalr", map =>
+                {
+                    map.UseCors(CorsOptions.AllowAll);
+
+                    var hubConfig = new HubConfiguration
+                    {
+                        EnableDetailedErrors = true,
+                        EnableJSONP = true
+                    };
+                    map.RunSignalR(hubConfig);
+                });
+            }
+        }
+
+        [HubName("MyHub")]
+        public class MyHub : Hub
+        {
+            public override Task OnConnected()
+            {
+                Console.WriteLine("Nueva Cliente: {0}", this.Context.ConnectionId.ToString());
+                return base.OnConnected();
+            }
+        }
+    }
+}
